2a31b3b8285457dd7e4c4457ee66f535c49c0497
Scroph <azi.hassan@live.fr>
2015-04-04 17:59:53 +0000

    Added the fizzbuzz example

---
+++ fizzbuzz/main.asm
@@ -0,0 +1,63 @@
+mov a, #0
+main:
+	mov p0, a
+	clr p2.0
+	clr p2.1
+fizz:
+	mov r0, a
+	mov r1, #3
+	call divisible
+	cjne r7, #1, buzz
+	setb P2.0
+buzz:
+	mov r0, a
+	mov r1, #5
+	call divisible
+	cjne r7, #1, backtomain
+	setb P2.1
+backtomain:
+	inc a
+	call delay
+	sjmp main
+
+delay:
+	push 0
+	push 1
+	push 2
+	mov r0, #255d
+	mov r1, #255d
+	mov r2, #4d
+
+delay_loop:
+	dec r0
+	cjne r0, #0, delay_loop
+	dec r1
+	cjne r1, #0, delay_loop
+	dec r2
+	cjne r2, #0, delay_loop
+	pop 2
+	pop 1
+	pop 0
+	ret
+
+;divider in r0
+;dividend in r1
+;the result will be stored in r7 : 1 if r0 % r1 == 0, 0 otherwise
+divisible:
+	push a
+	push b
+	mov a, r0
+	mov b, r1
+	div ab
+	;for some reason the B register doesn't work with the cjne instruction
+	;cjne b, #0, not_divisible
+	mov a, b ;we don't really care about the quotient
+	cjne a, #0, not_divisible
+	mov r7, #1
+	sjmp fin
+not_divisible:
+	mov r7, #0
+fin:
+	pop b
+	pop a
+	ret
---
+++ fizzbuzz/readme.md
@@ -0,0 +1,10 @@
+#Fizz Buzz in 8051 assembly
+
+The code is written in assembly, and is simulated in Proteus ISIS.
+
+
+## Circuit
+
+Two 7-segment displays are connected to the P0 port, displaying numbers from 0x00 to 0xFF. Two LEDs indicate whether the number is divisible by 3 (Fizz), 5 (Buzz), or both (Fizzbuzz). The "Fizz" and "Buzz" LEDs are connected to the P2.0 and P2.1 pins respectively.
+
+The 7-segment displays display the numbers in hexadecimal notation, so for example, 15 will only turn on the "Fizz" LED and not both, because 0x15 is the equivalent of the decimal number 21 which is only divisible by 3.
